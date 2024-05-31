dbcacce975bc81cbc4fb464e99fe8602350ed88d
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-05-30 22:41:01 -0400

    ZE FusionBot v2.5.0
    Latest distribution raid data added

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v2.4.9";
+        public const string Version = "v2.5.0";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
