5d70966c1e53186c1c4dc9c5ac0dd6e41789b6b6
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-06-09 02:44:28 -0400

    ZE FusionBot v2.6.6

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v2.6.0";
+        public const string Version = "v2.6.6";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
