7526daad972b7937f64f7137c7bac6bd450d1be7
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-10-25 03:12:53 -0400

    ZE FusionBot 3.4.5
    v3.4.5
    - Same as v3.4.4, but fixed batch trading being requested out of order and made them sequential.
    - Notes for v3.4.4: https://github.com/Secludedly/ZE-FusionBot/commit/a19f80b8d2ebf29adbaa3feb90a2e39f51bf333d

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v3.4.4";
+        public const string Version = "v3.4.5";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
