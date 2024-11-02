10de3bd17f4f19fd4ea4d9e44d1020f19d24241e
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-11-01 20:20:29 -0400

    ZE FusionBot v3.5.5
    - Updated for latest distribution data

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v3.5.4";
+        public const string Version = "v3.5.5";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
