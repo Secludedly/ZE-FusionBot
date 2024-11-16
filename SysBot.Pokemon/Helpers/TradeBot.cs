919fcd279930f9eaaf4d0f1ff239095e1be556c7
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-11-15 19:43:01 -0500

    revert alm, release v3.6.5 to fix v3.6.4
    v3.6.5

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v3.6.4";
+        public const string Version = "v3.6.5";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
