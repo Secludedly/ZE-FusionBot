d626455910f01501a37626087c08afc6df300804
Secludedly <GogoplataSteve@Gmail.com>
2025-02-23 22:29:41 -0500

    ZE FusionBot v3.7.4
    Latest distribution data

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v3.7.3";
+        public const string Version = "v3.7.4";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
