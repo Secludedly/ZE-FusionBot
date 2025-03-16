58868b4305b5516530faeb3df85f8ba351282f58
Secludedly <GogoplataSteve@Gmail.com>
2025-03-16 15:55:44 -0400

    ZE FusionBot v3.7.8
    Latest raid distributions.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v3.7.7";
+        public const string Version = "v3.7.8";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
