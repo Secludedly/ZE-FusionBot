53ae0aa847b05b785863d947e238bf712cf3079d
Secludedly <GogoplataSteve@Gmail.com>
2025-06-03 23:45:31 -0400

    SVGameVersion to 4.0.0
    v4.0.2

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v4.0.1";
+        public const string Version = "v4.0.2";
         public const string Attribution = "https://FreeMons.Org";
         public const string ConfigPath = "config.json";
     }
