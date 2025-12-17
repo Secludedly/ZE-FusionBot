a188b7d55d171f3677f4efae3026b892509c1059
Secludedly <GogoplataSteve@Gmail.com>
2025-12-17 05:31:30 -0500

    ZE FusionBot v6.8.9

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v6.7.9";
+        public const string Version = "v6.8.9";
     }
 }
