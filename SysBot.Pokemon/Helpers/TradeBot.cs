9057b1d82766a535e5d37c9d62d3a942e98e7f06
Secludedly <GogoplataSteve@Gmail.com>
2026-01-22 21:05:29 -0500

    Update Dependencies
    v7.3.9
    
    - PKHeX.Core.dll from 01-22-2026 (commit fe32739)
    - PKHeX.Core.AutoMod.dll from 01-21-2026 (commit 93675b7)
    
    In-line with PKHeX release version 26.01.22.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v7.3.8";
+        public const string Version = "v7.3.9";
     }
 }
