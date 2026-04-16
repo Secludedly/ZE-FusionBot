993cf7d3d91545a94d6a3c9137e1cb7849901729
Secludedly <GogoplataSteve@Gmail.com>
2026-04-16 05:49:23 -0400

    Show Tera Type for Non-Native Pokemon in SV
    v7.8.5
    
    - Non-Native Pokemon were not displaying their Tera Type in SV due to recent PKHeX changes. Converted the GameVersion.SL / GameVersion.VL to simply pull from PK9 instead to cover every Pokemon, regardless of origin.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v7.8.4";
+        public const string Version = "v7.8.5";
     }
 }
